596a1e2fb27c97862d7ae8b3710de15e1cdc4865
Wen Guan <wguan.icedew@gmail.com>
2022-08-04 18:40:18 +0200

    without directive

--- ngx_http_ssl_extension_module.c
+++ ngx_http_ssl_extension_module.c
@@ -8,26 +8,6 @@
 #include <ngx_http.h>
 
 
-typedef struct {
-    ngx_flag_t  ssl_allow_proxy;
-} ngx_http_ssl_extension_srv_conf_t;
-
-
-static void *
-ngx_http_ssl_extension_create_srv_conf(ngx_conf_t *cf)
-{
-    ngx_http_ssl_extension_srv_conf_t  *sscf;
-
-    sscf = ngx_pcalloc(cf->pool, sizeof(ngx_http_ssl_extension_srv_conf_t));
-    if (sscf == NULL) {
-        return NULL;
-    }
-
-    sscf->ssl_allow_proxy = NGX_CONF_UNSET;
-    return sscf;
-}
-
-
 static char *
 ngx_http_ssl_extension_allow_proxy(ngx_conf_t *cf)
 {
@@ -56,30 +36,10 @@ ngx_http_ssl_extension_merge_srv_conf(ngx_conf_t *cf, void *parent, void *child)
     ngx_http_ssl_extension_srv_conf_t *prev = parent;
     ngx_http_ssl_extension_srv_conf_t *conf = child;
 
-    ngx_conf_merge_value(conf->ssl_allow_proxy, prev->ssl_allow_proxy, 0);
-
-    if (conf->ssl_allow_proxy) {
-        ngx_http_ssl_extension_allow_proxy(cf);
-    }
+    ngx_http_ssl_extension_allow_proxy(cf);
     return NGX_CONF_OK;
 }
 
-
-
-//static ngx_conf_post_t  ngx_http_ssl_extension_allow_proxy_post = { ngx_http_ssl_extension_allow_proxy };
-
-
-static ngx_command_t  ngx_http_ssl_extension_commands[] = {
-
-    { ngx_string("ssl_allow_proxy"),
-      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_CONF_FLAG,
-      ngx_conf_set_flag_slot,
-      NGX_HTTP_SRV_CONF_OFFSET,
-      offsetof(ngx_http_ssl_extension_srv_conf_t, ssl_allow_proxy),
-      NULL },
-
-      ngx_null_command
-};
 
 
 static ngx_http_module_t  ngx_http_ssl_extension_module_ctx = {
@@ -89,7 +49,7 @@ static ngx_http_module_t  ngx_http_ssl_extension_module_ctx = {
     NULL,                                  /* create main configuration */
     NULL,                                  /* init main configuration */
 
-    ngx_http_ssl_extension_create_srv_conf,          /* create server configuration */
+    NULL,                                            /* create server configuration */
     ngx_http_ssl_extension_merge_srv_conf,           /* merge server configuration */
 
     NULL,                                  /* create location configuration */
@@ -100,7 +60,7 @@ static ngx_http_module_t  ngx_http_ssl_extension_module_ctx = {
 ngx_module_t  ngx_http_ssl_extension_module = {
     NGX_MODULE_V1,
     &ngx_http_ssl_extension_module_ctx,              /* module context */
-    ngx_http_ssl_extension_commands,                 /* module directives */
+    NULL,                                            /* module directives */
     NGX_HTTP_MODULE,                       /* module type */
     NULL,                                  /* init master */
     NULL,                                  /* init module */
